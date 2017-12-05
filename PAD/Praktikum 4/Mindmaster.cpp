bfd0ae162440cf81951aa5349e65696ff47d9e5f
Necromide <dobrutor2@web.de>
2017-12-05 22:24:46 +0100

    Update Mindmaster.cpp

--- PAD/Praktikum 4/Mindmaster.cpp
+++ PAD/Praktikum 4/Mindmaster.cpp
@@ -7,14 +7,14 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-    
+
     Mindmaster<int> obj;
-   
-    
-    int min = 0;        //Nur Ziffern von 0-9 sollen jeweils als Wert im Vector solution landen                                       
+
+
+    int min = 0; //Nur Ziffern von 0-9 sollen jeweils als Wert im Vector solution landen                                       
     int max = 9;
-    int len = 4;        //Der "Code" soll nur 4-Stellig sein
-    int versuche = 8;   //Anzahl an übrigen Versuchen
+    int len = 4; //Der "Code" soll nur 4-Stellig sein
+    int versuche = 8; //Anzahl an übrigen Versuchen
     vector<int> solution;
 
     cout << "Bitte geben sie die 4 Ziffern für ihren Code nacheinander ein." << endl;
@@ -30,10 +30,25 @@ int main(int argc, char** argv)
         cout << "Der eingegebene Code ist leider nicht zulässig." << endl;
         return 0;
     }
-    
+
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+    cout << "___________________________________________________________________________" << endl;
+
+
     for (int i = 0; i < 9; i++)
     {
-
+        versuche = versuche - 1;
         cout << "Geben sie die 4 Ziffern, die sie vermuten nacheinander ein." << endl;
         vector<int> guess;
         //Werte für guess erzeugen
@@ -46,13 +61,26 @@ int main(int argc, char** argv)
         if (obj.isValidInput(guess, len, min, max) == 0)
         {
             cout << "Der eingegebene Code ist leider nicht zulässig." << endl;
+            versuche = versuche + 1;
+        }
+        if (obj.isValidInput(guess, len, min, max) == 1)
+        {
+            cout << "Anzahl an schwarzen Zahlen: " << obj.black(solution, guess) << endl;
+            cout << "Anzahl an weißen Zahlen: " << obj.white(solution, guess) << endl;
+            cout << "Du hast noch " << versuche << " Versuche übrig." << endl;
+        }
+
+        if (obj.black(solution, guess) == 4)
+        {
+            cout << "Glückwunsch du hast den Code erraten!" << endl;
             return 0;
         }
 
-        cout << "Anzahl an schwarzen Zahlen: " << obj.black(solution, guess) << endl;
-        cout << "Anzahl an weißen Zahlen: " << obj.white(solution, guess) << endl;
-
-        return 0;
+        if (versuche == 0)
+        {
+            cout << "Schade, du hast alle Versuche verbraucht!" << endl;
+            return 0;
+        }
     }
     return 0;
 }
